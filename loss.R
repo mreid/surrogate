10a7e22d38550b07c527f2c71e8e76cae1336a4a
mreid <mark@reid.name>
2009-02-21 16:45:37 +1100

    Debugging

--- loss.R
+++ loss.R
@@ -1,6 +1,9 @@
+source("Deriv.R")
+
 # Cost weighted loss generator
 lcost <- function(c) { 
-	function(y,e) y*(1-c)*as.real(e<c) + (1-y)*c*as.real(e>=c)
+#	function(y,e) ifelse(e<c, y*(1-c), (1-y)*c)
+	function(y,e) as.real(e<c)*y*(1-c) + as.real(e>=c)*(1-y)*c
 }
 
 # 0-1 Loss
@@ -13,20 +16,33 @@ lsquare <- function(y,e) y*(1-e)^2 + (1-y)*e^2
 llog <- function(y,e) -y*log(e) - (1-y)*log(1-e)
 
 # Exponential Loss
+# Hand calculated expressions to test against those computed with `bound`
 lexp <- function(y,e) y*sqrt((1-e)/e) + (1-y)*sqrt(e/(1-e)) 
+minlexp <- function(y) y*sqrt((1-y)/y) + (1-y)*sqrt(y/(1-y))
+dminlexp <- function(y) (1-2*y)/sqrt(y*(1-y))
+Bexp <- function(y,e) lexp(y,e) - minlexp(y)
+psiexp <- function(c, alpha) minlexp(c) - minlexp(c-alpha) + alpha*dminlexp(c)
+# boundexp <- function(c, alpha) min(psiexp(c,alpha), psiexp(c,-alpha))
+boundexp <- function(y,e) {
+	    l05 <- lcost(0.5)
+		alpha <- l05(y,e) - l05(y,y)
+		min(psiexp(0.5, alpha), psiexp(0.5, -alpha))
+}
 
 # Operator that computes point-wise Bayes loss from loss
 Lmin <- function(loss) function(y) loss(y,y)
 
-# Opertator to compute the regret function for a loss
+# Operator to compute the regret function for a loss
 B <- function(loss) function(y,e) loss(y,e) - Lmin(loss)(y)
 
-psi <- function(loss, c) {
-	function(y,e) {
-		alpha <- B(lcost(c))(y,e)
-		Lmin(loss)(c) - Lmin
-	}
+psi <- function(loss, c, alpha) {
+	minloss <- Lmin(loss)
+	dminloss <- Deriv.function(minloss)
+	minloss(c) - minloss(c-alpha) + alpha*dminloss(c)
 }
 
-# Example use of expressions and automatic differentiation 
-eval(deriv(expression(3*x^2 + 2), "x"), environment(x <- 0.3))[1]
+bound <- function(loss, c) {
+	function(alpha) {
+		min(psi(loss, c, alpha), psi(loss, c, -alpha))
+	}
+}
