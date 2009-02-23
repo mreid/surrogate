9c2ada1f71ecb84ecd9e6e66004635ed611377b2
Mark Reid <mark@threewordslong.com>
2009-02-23 16:37:28 +1100

    Clean up

--- loss.R
+++ loss.R
@@ -30,45 +30,10 @@ psi <- function(loss, c, alpha) {
 	minloss(c) - minloss(c-alpha) + alpha*dminloss(c)
 }
 
-bound <- function(loss, c) {
-	Vectorize(function(alpha) {
-		min(psi(loss, c, alpha), psi(loss, c, -alpha))
-	})
-}
-
-bound2 <- function(loss, c) {
+plotBounds <- function(loss, c, est, title=deparse(body(loss))) {
 	Bc <- B(lcost(c))
-	function(y,e) {
-		alpha = Bc(y,e)
-		max(psi(loss, c, alpha), psi(loss,c,-alpha))
-	}
-}
-
-# Plot some bounds as a function of alpha
-bsq <- bound(lsquare, 0.5)
-bexp <- bound(lexp, 0.5)
-blog <- bound(llog, 0.5)
-
-plot(bsq, 0, 0.5, ylim=c(0,1))
-plot(bexp, add=TRUE, col='red')
-plot(blog, add=TRUE, col='blue')
-
-c <- 0.7
-b2sq <- Vectorize(bound2(lsquare, c))
-b2exp <- Vectorize(bound2(lexp, c))
-b2log <- Vectorize(bound2(llog, c))
-bc <- B(lcost(c))		# The true regret of l_{0.5} (i.e., alpha)
-
-est <- 0.4
-plot(function(y) bc(y,est), 0, 1, ylim=c(0,1), lty=2)
-plot(function(y) b2sq(y,est), add=TRUE)
-plot(function(y) b2exp(y,est), add=TRUE, col='red')
-plot(function(y) b2log(y,est), add=TRUE, col='blue')
-
-plotBounds <- function(loss, c, est) {
-	Bc <- B(lcost(c))
-	bloss <- Vectorize(bound2(loss, c))
-	plot(function(y) Bc(y,est), 0, 1, ylim=c(-1,1), lty=2)
+	Bloss <- B(loss)
+	plot(function(y) Bloss(y,est), 0, 1, ylim=c(-0.5,1), lty=1, main=title, ylab='Regret', xlab='η')
 	plot(function(y) psi(loss, c, Bc(y,est)), add=TRUE, col='red')
 	plot(function(y) psi(loss, c, -Bc(y,est)), add=TRUE, col='red', lty=2)
 }
